ed8ddee00fdac3143f83656900d205695d7c097e
Kristen <kristen.s.yang@gmail.com>
2018-03-09 12:41:49 -0800

    first commit

---
+++ EmailDatabaseApplication/EmailDatabase/DeleteEmail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EmailDatabase
+{
+    class DeleteEmail
+    {
+        private string firstName;
+        private string lastName;
+        private string password;
+        private int defaultPasswordLength = 8;
+        private string department;
+        private string mailboxCapacity;
+        private string companySuffix = "sillycompany.com";
+
+        //Constructor to receive the first and last name
+        public DeleteEmail(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+
+            //write to database
+            Database deleteDatabase = new Database();
+            deleteDatabase.Delete(this.firstName, this.lastName);
+        }
+    }
+}
